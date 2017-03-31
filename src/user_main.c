fcf8f617d52da4fcbbe4159e0b5490d338cc3609
Mateusz Stojek <mateusz@stojek.eu>
2017-03-31 23:53:31 +0200

    Add connect to wifi

--- src/user_main.c
+++ src/user_main.c
@@ -7,25 +7,39 @@
 #define user_procTaskPrio        0
 #define user_procTaskQueueLen    1
 os_event_t    user_procTaskQueue[user_procTaskQueueLen];
-static void loop(os_event_t *events);
 
 //Main code function
-static void ICACHE_FLASH_ATTR
-loop(os_event_t *events)
+static void ICACHE_FLASH_ATTR loop(os_event_t *events)
 {
-    os_printf("Hello\n\r");
-    os_delay_us(1000000);
-    system_os_post(user_procTaskPrio, 0, 0 );
+    system_os_post(user_procTaskPrio, 0, 0);
+}
+
+bool ICACHE_FLASH_ATTR user_set_station_config(char *ssid, char *password)
+{
+    struct station_config stationConf;
+    stationConf.bssid_set = 0;      //need not check MAC address of AP
+
+    os_memset(&stationConf.ssid, 0, sizeof(stationConf.ssid));
+    os_memset(&stationConf.password, 0, sizeof(stationConf.password));
+
+    os_memcpy(&stationConf.ssid, ssid, os_strlen(ssid));
+    os_memcpy(&stationConf.password, password, os_strlen(password));
+
+    s_printf("%s\n\r", __FUNCTION__);
+    return wifi_station_set_config(&stationConf);
 }
 
 //Init function 
-void ICACHE_FLASH_ATTR
-user_init()
+void ICACHE_FLASH_ATTR user_init()
 {
-    uart_div_modify( 0, UART_CLK_FREQ / ( UART_SPEED ) );
-    
+    uart_div_modify(0, UART_CLK_FREQ / (UART_SPEED));
+
+    wifi_set_opmode(STATION_MODE);
+
+    // Uncoment bellow line when you want to set new wifi settings
+    // user_set_station_config(WIFI_SSID, WIFI_PASSWORD);
+
     //Start os task
     system_os_task (loop, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
-
     system_os_post (user_procTaskPrio, 0, 0 );
 }
